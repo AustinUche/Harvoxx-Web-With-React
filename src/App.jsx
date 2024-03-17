5bb36c963a68b1331ce4e7004be16d68f2ed6ddd
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-03-17 20:35:51 +0100

    added the price child component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Prices from "./components/Prices";
 import Services from "./components/Services";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Prices />
     </>
   );
 }
